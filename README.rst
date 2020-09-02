c9c717aa5848fe7989488cc44a3f845594ca585d
Olayanju Ajibola <ajibolaolayanju@gmail.com>
2020-09-02 23:48:42 +0100

    create monnify class

---
+++ README.rst
@@ -0,0 +1,24 @@
+=====
+Python Monnify
+=====
+
+This library makes it easy to connect with Monnify API (To create reserve accounts)
+
+It will be updated and maintained as needed
+
+Detailed documentation is in the "docs" directory.
+
+Quick start
+-----------
+1. pip install requests python_monnify
+
+2. To use from monnify import Monnify
+
+3. method includes 
+	> generateToken: Method generate Token to be used in the subsequent calls
+	> createReserveAccount: Creates reserve accounts and returns value from monnify's API
+	> createHashFromWebhook: Generate hash as described on monnify's documentation to compare with transactionsHash
+4. #=========== USAGE ==========
+	x = Monnify("apiKey", "clientSecretKey")
+	print(x.generateToken())
+	print(x.createReserveAccount())
---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='python_monnify',
+    version='0.1',
+    zip_safe=False,
+    include_package_data=True,
+    packages=find_packages(),
+    license='MIT License',  # example license
+    description='A Library created for ease of using monnify\'s API to create unique account numbers for your users/members',
+    long_description_content_type='text/plain',
+    long_description=README,
+    url='https://github.com/ldonjibson',
+    author='Olayanju A. Ajibola',
+    author_email='vtubusinessportal@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
